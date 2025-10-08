7572a7431ddc73d33edd799e2ba7fff33b30cfec
jonathan_uber <jonathanuber121207@gmail.com>
2025-10-07 21:48:19 -0300

    refactor: refactor packages

---
+++ src/main/java/org/maintenancesystem/application/exceptions/ExcpetionTeste.java
@@ -0,0 +1,7 @@
+package org.maintenancesystem.application.exceptions;
+
+public class ExcpetionTeste extends RuntimeException {
+    public ExcpetionTeste(String message) {
+        super(message);
+    }
+}
